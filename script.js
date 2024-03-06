9ee7fb0b9b75d2983e726114c92aada1d9d0d6c1
Saju14 <87143546+Saju14@users.noreply.github.com>
2024-03-06 20:35:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+var map = L.map('map').setView([10.903219337541, 76.43448118177776], 13);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Leaflet | © OpenStreetMap contributors'
+}).addTo(map);
+
+var marker;
+map.on('click', function (event) {
+    if (marker) {
+        map.removeLayer(marker);
+    }
+    marker = L.marker(event.latlng).addTo(map);
+});

7c3e2fc7f1cdec5cee345f6a7eb677aa86dd9f55
Azhar Abdulla C K <146507585+azru007@users.noreply.github.com>
2025-01-15 21:07:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 3000;
 
 const mimeTypes = {
   '.html': 'text/html',
